17acfe293d261fc30340400e7b4cb31bb11ba5ed
Vittorio Lo Preiato <vittoriol@macBookProFalconeta.local>
2018-04-05 12:58:09 +0200

    risolto ! nel routing

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,7 +19,8 @@ angular
     'ngSanitize',
     'ngTouch'
   ])
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(false).hashPrefix('');
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
